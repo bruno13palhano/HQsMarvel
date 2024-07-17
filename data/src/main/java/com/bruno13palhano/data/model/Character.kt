7189b6b3a612f10df76b4e58aa6357f47135b23a
bruno13palhano <bruno13palhano@gmail.com>
2024-07-17 14:27:02 -0300

    Add Character and Comic models

---
+++ data/src/main/java/com/bruno13palhano/data/model/Character.kt
@@ -0,0 +1,8 @@
+package com.bruno13palhano.data.model
+
+data class Character(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val thumbnail: String
+)
---
+++ data/src/main/java/com/bruno13palhano/data/model/Comic.kt
@@ -0,0 +1,8 @@
+package com.bruno13palhano.data.model
+
+data class Comic(
+    val id: Long,
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)

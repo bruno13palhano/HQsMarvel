c77005842770dd6d94c4d6d4a7dfb1ed91a776f5
bruno13palhano <bruno13palhano@gmail.com>
2024-07-20 12:51:55 -0300

    Add ComicsRemoteMediator

---
+++ data/src/main/java/com/bruno13palhano/data/repository/comics/ComicsRemoteMediator.kt
@@ -0,0 +1,108 @@
+package com.bruno13palhano.data.repository.comics
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import com.bruno13palhano.data.local.database.HQsMarvelDatabase
+import com.bruno13palhano.data.model.Comic
+import com.bruno13palhano.data.model.RemoteKeys
+import com.bruno13palhano.data.remote.datasource.comics.ComicRemoteDataSource
+import okio.IOException
+import retrofit2.HttpException
+import java.util.concurrent.TimeUnit
+
+@OptIn(ExperimentalPagingApi::class)
+internal class ComicsRemoteMediator(
+    private val limit: Int = 15,
+    private val database: HQsMarvelDatabase,
+    private val comicRemoteDataSource: ComicRemoteDataSource
+) : RemoteMediator<Int, Comic>() {
+    override suspend fun initialize(): InitializeAction {
+        val cacheTimeout = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
+
+        return if (System.currentTimeMillis() - (database.remoteKeysDao.getCreationTime() ?: 0) < cacheTimeout) {
+            InitializeAction.SKIP_INITIAL_REFRESH
+        } else {
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+        }
+    }
+
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, Comic>
+    ): MediatorResult {
+        val page: Int =
+            when (loadType) {
+                LoadType.REFRESH -> {
+                    val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                    remoteKeys?.nextKey?.minus(1) ?: 1
+                }
+                LoadType.PREPEND -> {
+                    val remoteKeys = getRemoteKeyForFirstItem(state)
+                    val prevKey = remoteKeys?.prevKey
+                    prevKey ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                }
+                LoadType.APPEND -> {
+                    val remoteKeys = getRemoteKeyForLastItem(state)
+                    val nextKey = remoteKeys?.nextKey
+                    nextKey ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                }
+            }
+
+        try {
+            val offset = (page - 1) * limit
+
+            val response = comicRemoteDataSource.getComics(offset, limit)
+            val endOfPaginationReached = response.isEmpty()
+
+            database.withTransaction {
+                if (loadType == LoadType.REFRESH) {
+                    database.remoteKeysDao.clearRemoteKeys()
+                    database.comicsDao.clearComics()
+                }
+                val prevKey = if (page > 1) page - 1 else null
+                val next = if (endOfPaginationReached) null else page + 1
+                val remoteKeys =
+                    response.map {
+                        RemoteKeys(
+                            comicId = it.comicId,
+                            prevKey = prevKey,
+                            currentPage = page,
+                            nextKey = next,
+                            createAt = System.currentTimeMillis()
+                        )
+                    }
+                database.remoteKeysDao.insertAll(remoteKeys = remoteKeys)
+                database.comicsDao.insertAll(comics = response.map { it.copy(page = page) })
+            }
+
+            return MediatorResult.Success(endOfPaginationReached = endOfPaginationReached)
+        } catch (e: IOException) {
+            return MediatorResult.Error(e)
+        } catch (e: HttpException) {
+            return MediatorResult.Error(e)
+        }
+    }
+
+    private suspend fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Int, Comic>): RemoteKeys? {
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.id?.let { id ->
+                database.remoteKeysDao.remoteKeyId(id = id)
+            }
+        }
+    }
+
+    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, Comic>): RemoteKeys? {
+        return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()?.let { comic ->
+            database.remoteKeysDao.remoteKeyId(id = comic.id)
+        }
+    }
+
+    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, Comic>): RemoteKeys? {
+        return state.pages.lastOrNull { it.data.isNotEmpty() }?.data?.lastOrNull()?.let { comic ->
+            database.remoteKeysDao.remoteKeyId(id = comic.id)
+        }
+    }
+}

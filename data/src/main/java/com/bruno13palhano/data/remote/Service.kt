ff6934707727902b6e5fd1cb8de0d4a6db62c497
bruno13palhano <bruno13palhano@gmail.com>
2024-07-21 00:23:59 -0300

    Refactor

--- data/src/main/java/com/bruno13palhano/data/remote/Service.kt
+++ data/src/main/java/com/bruno13palhano/data/remote/Service.kt
@@ -7,16 +7,16 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-private const val params = "?&ts=1&apikey=${BuildConfig.apiPublicKey}&hash=${BuildConfig.apiHash}"
+private const val PARAMS = "ts=1&apikey=${BuildConfig.apiPublicKey}&hash=${BuildConfig.apiHash}"
 
 internal interface Service {
-    @GET("comics$params")
+    @GET("comics?$PARAMS")
     suspend fun getComics(
         @Query("offset") offset: Int,
         @Query("limit") limit: Int
     ): ComicDataWrapper
 
-    @GET("comics/{comicId}/characters$params")
+    @GET("comics/{comicId}/characters?$PARAMS")
     suspend fun getCharacters(
         @Path("comicId") id: Long,
         @Query("offset") offset: Int,

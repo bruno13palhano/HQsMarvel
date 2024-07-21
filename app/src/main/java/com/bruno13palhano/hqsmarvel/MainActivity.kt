7a094bbebc797575a3d0bb160ca8c7863b210500
bruno13palhano <bruno13palhano@gmail.com>
2024-07-21 15:26:28 -0300

    Set screens navigation

--- app/src/main/java/com/bruno13palhano/hqsmarvel/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/hqsmarvel/MainActivity.kt
@@ -3,45 +3,68 @@ package com.bruno13palhano.hqsmarvel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.bruno13palhano.hqsmarvel.ui.common.BottomMenu
+import com.bruno13palhano.hqsmarvel.ui.navigation.MainNavGraph
 import com.bruno13palhano.hqsmarvel.ui.theme.HQsMarvelTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             HQsMarvelTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                var showBottomMenu by rememberSaveable { mutableStateOf(true) }
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        bottomBar = {
+                            AnimatedVisibility(
+                                visible = showBottomMenu,
+                                enter =
+                                    slideInVertically(
+                                        animationSpec = spring(stiffness = Spring.StiffnessHigh),
+                                        initialOffsetY = { it / 8 }
+                                    ),
+                                exit =
+                                    slideOutVertically(
+                                        animationSpec = spring(stiffness = Spring.StiffnessHigh),
+                                        targetOffsetY = { it / 8 }
+                                    )
+                            ) {
+                                BottomMenu(navController = navController)
+                            }
+                        }
+                    ) { innerPadding ->
+                        MainNavGraph(
+                            modifier = Modifier.padding(innerPadding),
+                            navController = navController,
+                            showBottomMenu = { show -> showBottomMenu = show }
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HQsMarvelTheme {
-        Greeting("Android")
     }
 }

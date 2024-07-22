49e579f39bfbbad1e80a8303125dfbfc1e96a6a0
bruno13palhano <bruno13palhano@gmail.com>
2024-07-22 10:14:27 -0300

    Add a mehtod to create md5 hash

--- data/src/main/java/com/bruno13palhano/data/remote/Service.kt
+++ data/src/main/java/com/bruno13palhano/data/remote/Service.kt
@@ -1,22 +1,19 @@
 package com.bruno13palhano.data.remote
 
-import com.bruno13palhano.data.BuildConfig
 import com.bruno13palhano.data.remote.model.character.CharacterDataWrapper
 import com.bruno13palhano.data.remote.model.comics.ComicDataWrapper
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-private const val PARAMS = "ts=${BuildConfig.apiTs}&apikey=${BuildConfig.apiPublicKey}&hash=${BuildConfig.apiHash}"
-
 internal interface Service {
-    @GET("comics?$PARAMS")
+    @GET("comics")
     suspend fun getComics(
         @Query("offset") offset: Int,
         @Query("limit") limit: Int
     ): ComicDataWrapper
 
-    @GET("comics/{comicId}/characters?$PARAMS")
+    @GET("comics/{comicId}/characters")
     suspend fun getCharacters(
         @Path("comicId") id: Long,
         @Query("offset") offset: Int,
--- data/src/main/java/com/bruno13palhano/data/remote/di/ServiceModule.kt
+++ data/src/main/java/com/bruno13palhano/data/remote/di/ServiceModule.kt
@@ -12,6 +12,9 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.security.MessageDigest
+import java.util.Calendar
+import java.util.TimeZone
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -28,23 +31,43 @@ internal object ServiceModule {
 
         val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
 
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logger)
+
+        httpClient.addInterceptor { chain ->
+            val original = chain.request()
+            val baseUrl = original.url
+            val ts = (Calendar.getInstance(TimeZone.getTimeZone("UTC")).timeInMillis / 1000L).toString()
+
+            val url =
+                baseUrl.newBuilder()
+                    .addQueryParameter("ts", ts)
+                    .addQueryParameter("apikey", BuildConfig.apiPublicKey)
+                    .addQueryParameter("hash", md5("$ts${BuildConfig.apiPrivateKey}${BuildConfig.apiPublicKey}"))
+                    .build()
+
+            chain.proceed(original.newBuilder().url(url).build())
+        }
+            .readTimeout(300, TimeUnit.SECONDS)
+            .connectTimeout(300, TimeUnit.SECONDS)
+            .writeTimeout(300, TimeUnit.SECONDS)
+
         val retrofit =
             Retrofit.Builder()
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
                 .baseUrl(BuildConfig.baseUrl)
-                .client(
-                    OkHttpClient
-                        .Builder()
-                        .addInterceptor(logger)
-                        .readTimeout(300, TimeUnit.SECONDS)
-                        .connectTimeout(300, TimeUnit.SECONDS)
-                        .writeTimeout(300, TimeUnit.SECONDS)
-                        .build()
-                )
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .client(httpClient.build())
                 .build()
 
         val apiService: Service by lazy { retrofit.create(Service::class.java) }
 
         return apiService
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun md5(input: String): String {
+        val md = MessageDigest.getInstance("MD5")
+        val digest = md.digest(input.toByteArray())
+        return digest.toHexString()
     }
 }

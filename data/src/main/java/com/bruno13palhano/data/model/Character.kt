a8a8bf913dc71452b0fe548ae5b3d76a6c7223e0
bruno13palhano <bruno13palhano@gmail.com>
2024-07-24 01:38:28 -0300

    Make Character an entity

--- data/src/main/java/com/bruno13palhano/data/model/Character.kt
+++ data/src/main/java/com/bruno13palhano/data/model/Character.kt
@@ -1,8 +1,18 @@
 package com.bruno13palhano.data.model
 
+import androidx.room.Entity
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Entity(tableName = "Character", primaryKeys = ["id"])
 data class Character(
+    @SerialName("id")
     val id: Long,
+    @SerialName("name")
     val name: String?,
+    @SerialName("description")
     val description: String?,
+    @SerialName("thumbnail")
     val thumbnail: String?
 )

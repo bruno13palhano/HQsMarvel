2cf1238ef6d861953a67ca68dd12ffc6632df499
bruno13palhano <bruno13palhano@gmail.com>
2024-07-25 20:45:33 -0300

    Doc api calls

--- data/src/main/java/com/bruno13palhano/data/remote/Service.kt
+++ data/src/main/java/com/bruno13palhano/data/remote/Service.kt
@@ -6,13 +6,29 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
+/**
+ * Interface for fetching data from API.
+ */
 internal interface Service {
+    /**
+     * @param offset The offset for the query.
+     *
+     * @param limit The limit for the query. Must be greater than 0
+     * and equal to or less than 100.
+     *
+     * @return a [ComicDataWrapper].
+     */
     @GET("comics")
     suspend fun getComics(
         @Query("offset") offset: Int,
         @Query("limit") limit: Int
     ): ComicDataWrapper
 
+    /**
+     * @param id The Character's id for the query.
+     *
+     * @return a [CharacterDataWrapper].
+     */
     @GET("characters/{id}")
     suspend fun getCharacter(
         @Path("id") id: Long
